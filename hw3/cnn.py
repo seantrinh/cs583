6294128e90927ad8abea4ddd910ca10a36ec771e
Sean Trinh <strinh@stevens.edu>
2019-04-01 16:14:22 -0400

    changes

---
+++ hw3/cnn.py
@@ -0,0 +1,70 @@
+#PLEASE DISREGARD
+from keras.datasets import cifar10
+import numpy
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+print('shape of x_train: ' + str(x_train.shape))
+print('shape of y_train: ' + str(y_train.shape))
+print('shape of x_test: ' + str(x_test.shape))
+print('shape of y_test: ' + str(y_test.shape))
+print('number of classes: ' + str(numpy.max(y_train) - numpy.min(y_train) + 1))
+
+def to_one_hot(y, num_class=10):
+    vect = []
+    for item in y:
+        x = [0]*item[0] + [1] + [0]*(num_class - 1 - item[0])
+        vect += [x]
+    return numpy.array(vect)
+
+y_train_vec = to_one_hot(y_train)
+y_test_vec = to_one_hot(y_test)
+
+print('Shape of y_train_vec: ' + str(y_train_vec.shape))
+print('Shape of y_test_vec: ' + str(y_test_vec.shape))
+
+print(y_train[0])
+print(y_train_vec[0])
+
+rand_indices = numpy.random.permutation(50000)
+train_indices = rand_indices[0:40000]
+valid_indices = rand_indices[40000:50000]
+
+x_val = x_train[valid_indices, :]
+y_val = y_train_vec[valid_indices, :]
+
+x_tr = x_train[train_indices, :]
+y_tr = y_train_vec[train_indices, :]
+
+print('Shape of x_tr: ' + str(x_tr.shape))
+print('Shape of y_tr: ' + str(y_tr.shape))
+print('Shape of x_val: ' + str(x_val.shape))
+print('Shape of y_val: ' + str(y_val.shape))
+
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.models import Sequential
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+from keras import optimizers
+
+learning_rate = 1E-5 # to be tuned!
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=learning_rate),
+              metrics=['acc'])
+
+history = model.fit(x_tr, y_tr, batch_size=32, epochs=10, validation_data=(x_val, y_val))
+
+loss_and_acc = model.evaluate(x_test, y_test_vec)
+print('loss = ' + str(loss_and_acc[0]))
+print('accuracy = ' + str(loss_and_acc[1]))
